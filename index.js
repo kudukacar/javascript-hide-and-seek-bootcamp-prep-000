ffcdda40e378b4a7907c20db82b26181673e81fb
Manju Thomas <kudukacar@hotmail.com>
2018-09-03 23:17:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementByID('nested').querySelector(".target");
+}
+function increaseRankBy(n) {
+  var lis = document.getElementByID('app').querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
+function deepestChild(){
+  var lis = document.getElementByID('grand-node').querySelectorAll('div');
+  var test;
+  for (i=0; i < lis.length; i++) {
+    test = lis[i].querySelector('div');
+  }
+  return test;
+}

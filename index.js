4aeca7752d6509856393b33b2743033d73e7a002
Manju Thomas <kudukacar@hotmail.com>
2018-09-03 21:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function nestedTarget() {
   return document.getElementByID('nested').querySelector(".target");
 }
 function increaseRankBy(n) {
-  var lis = document.getElementByID('app').querySelectorAll("ul.ranked-list li")
+  var lis = document.getElementByID('app').querySelectorAll("ul.ranked-list li");
 }
